2d438d9e031bc8b92bfc9e0c21173185683f5af4
jineeKim <jin03hee16@gmail.com>
2018-12-26 22:38:49 +0900

    home fragment

--- Appjam/app/src/main/java/com/example/dohee/ssgsag/MainActivity.kt
+++ Appjam/app/src/main/java/com/example/dohee/ssgsag/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.View
 import android.widget.RelativeLayout
 import com.example.dohee.ssgsag.Adapter.MyFragmentStatePagerAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,17 +16,21 @@ class MainActivity : AppCompatActivity() {
         configureBottomNavigation()
     }
 
+
     private fun configureBottomNavigation() {
         vp_main_act_view_frag_pager.adapter = MyFragmentStatePagerAdapter(supportFragmentManager, 3) //3개를 고정시키겠다.
         vp_main_act_view_frag_pager.offscreenPageLimit = 3
-        vp_main_act_view_frag_pager.setCurrentItem(1,true)
+        vp_main_act_view_frag_pager.setCurrentItem(1, true)
         // ViewPager와 Tablayout을 엮어줍니다!
         tl_top_navi_act_top_menu.setupWithViewPager(vp_main_act_view_frag_pager)
         //TabLayout에 붙일 layout을 찾아준 다음
         val bottomNaviLayout: View = this.layoutInflater.inflate(R.layout.activity_top_navi_avtivity, null, false)
         //탭 하나하나 TabLayout에 연결시켜줍니다.
-        tl_top_navi_act_top_menu.getTabAt(0)!!.customView = bottomNaviLayout.findViewById(R.id.btn_top_navi_my_page_tab) as RelativeLayout
-        tl_top_navi_act_top_menu.getTabAt(1)!!.customView = bottomNaviLayout.findViewById(R.id.btn_top_navi_home_tab) as RelativeLayout
-        tl_top_navi_act_top_menu.getTabAt(2)!!.customView = bottomNaviLayout.findViewById(R.id.btn_top_navi_calendar_tab) as RelativeLayout
+        tl_top_navi_act_top_menu.getTabAt(0)!!.customView =
+                bottomNaviLayout.findViewById(R.id.btn_top_navi_my_page_tab) as RelativeLayout
+        tl_top_navi_act_top_menu.getTabAt(1)!!.customView =
+                bottomNaviLayout.findViewById(R.id.btn_top_navi_home_tab) as RelativeLayout
+        tl_top_navi_act_top_menu.getTabAt(2)!!.customView =
+                bottomNaviLayout.findViewById(R.id.btn_top_navi_calendar_tab) as RelativeLayout
     }
 }
