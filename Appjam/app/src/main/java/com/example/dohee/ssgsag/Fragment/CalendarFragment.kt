c3805a31de1608126a1e4547c2216d05f1e68fcc
hyewon <hyewon2810@gmail.com>
2018-12-27 18:38:46 +0900

    dot

--- Appjam/app/src/main/java/com/example/dohee/ssgsag/Fragment/CalendarFragment.kt
+++ Appjam/app/src/main/java/com/example/dohee/ssgsag/Fragment/CalendarFragment.kt
@@ -1,5 +1,6 @@
 package com.example.dohee.ssgsag.Fragment
 
+import android.graphics.Color
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.util.Log
@@ -8,10 +9,13 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import com.applandeo.materialcalendarview.EventDay
+import com.example.dohee.ssgsag.Decorator.DotDecorator
+import com.example.dohee.ssgsag.Decorator.EventDecorator
 import com.example.dohee.ssgsag.Decorator.OneDayDecorator
 import com.example.dohee.ssgsag.Decorator.SundayDecorator
 import com.example.dohee.ssgsag.R
 import com.prolificinteractive.materialcalendarview.CalendarDay
+import com.prolificinteractive.materialcalendarview.DayViewFacade
 import kotlinx.android.synthetic.main.fragment_calendar.*
 import kotlinx.android.synthetic.main.fragment_calendar.view.*
 import java.util.*
@@ -50,17 +54,19 @@ class CalendarFragment : Fragment(){
    */
 
     fun setOnClickListener(calendarFragment: View){
+
+
         calendarFragment.frag_calendar_view.setOnDateChangedListener { widget, date, selected ->
+
+            var dotdecorator = DotDecorator(date)
+
             var year = date.year
             var month = date.month+1
             var day = date.day
             var today:String = year.toString()+"년"+month.toString()+"월"+day.toString()+"일"
             Toast.makeText(context, today, Toast.LENGTH_SHORT).show()
             Log.e("log test1", date.toString())
-
-            //var event = EventDay
-            //val calendar1 = Calendar.getInstance()
-            //event.add
+            widget.addDecorator(dotdecorator)
 
 
 
