565cb2c028f9bc208586df8e26ce96e0d47316d1
her0807 <her0807@naver.com>
2018-12-27 18:51:02 +0900

    TimeHelper 생성
    18/12/27

---
+++ Server/src/main/java/org/sopt/ssgsag/utils/TimeHelper.java
@@ -0,0 +1,11 @@
+package org.sopt.ssgsag.utils;
+
+import java.util.Date;
+
+public class TimeHelper {
+    // 타입별로 로컬시간 리턴을 받고 싶을때 사용
+    // ex 날짜타입: 'YYYYMMDD', 월: 'MM', 일: 'DD', 시 : 'HH', 분:'mm', 초:'ss'
+
+    }
+
+
