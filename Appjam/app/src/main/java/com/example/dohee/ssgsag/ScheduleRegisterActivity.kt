e63d2fa6993bed3f67790a036f5c3f62dc892df5
LAPTOP-IMB0SKGL\kdhee <kdheejb7@gmail.com>
2018-12-28 20:28:32 +0900

    schedule register activity

---
+++ Appjam/app/src/main/java/com/example/dohee/ssgsag/ScheduleRegisterActivity.kt
@@ -0,0 +1,13 @@
+package com.example.dohee.ssgsag
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ScheduleRegisterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_schedule_register)
+    }
+    
+}
